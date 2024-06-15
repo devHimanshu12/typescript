f20b12aecf45127e18d92a607c8f2ad3e1bce146
himanshu <shimanshu94@gmail.com>
2024-06-15 21:33:38 +0530

    notes on typescript

---
+++ intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Himanshu Shah", age: 18 };
+console.log("Himanshu Sah");
+console.log(user.name);
---
+++ intro/intro.ts
@@ -0,0 +1,4 @@
+const user = {name:"Himanshu Shah",age:18}
+
+console.log("Himanshu Sah")
+console.log(user.name)
